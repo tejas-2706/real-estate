d4e05f6042bf279e6196e59a8191bd83e01e14e2
TejasPangaonkar <pangaonkar.svc424@gmail.com>
2024-07-25 23:22:55 +0530

    new login setup and header added

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -10,3 +10,4 @@ const Provider = ({children}) => {
 }
 
 export default Provider
+
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
-const Header = () => {
+'use client'
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import Link from "next/link";
+function Header(){
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, [])
+  
   return (
-    <div>
-      <Image src={'/logo.svg'} width={120} height={120} alt='logo'></Image>
+    <div className="p-2 px-10 flex justify-between shadow-sm bg-fixed top-0 w-full z-10 bg-white">
+      <div className="flex gap-10 items-center">
+        <Image src={"/logo.svg"} width={80} height={80} alt="logo"></Image>
+        <ul className="hidden md:flex gap-10">
+          <Link href={'/'}><li className={`'hover:bg-slate-100 px-4 p-2 cursor-pointer rounded-lg' ${path=='/' && 'bg-slate-200 rounded-lg'}`}>Sale</li></Link>
+          <Link href={'/'}><li className="hover:bg-slate-100 px-4 p-2 cursor-pointer rounded-lg">Rent</li></Link>
+          <Link href={'/'}><li className="hover:bg-slate-100 px-4 p-2 cursor-pointer rounded-lg">Agent finder</li></Link>
+          
+        </ul>
+      </div>
+      <div className="flex gap-2 ">
+        <Button className='flex gap-2'>Post<Plus className="h-5 w-5"/></Button>
+        <Link href={'/login'}><Button className='bg-white border text-black hover:bg-slate-200' variant="outline">Login</Button></Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
